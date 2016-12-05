cc306161f66f782dcbec5d8f81fa05dbeaffffa3
hong <hong_zhang@foxitsoftware.com>
2016-12-05 14:19:19 -0800

    Initial Commit

---
+++ iosRDKAppendPageViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosRDKAppendPageViewDemo
+//
+//  Created by foxit on 12/5/16.
+//  Copyright © 2016 Foxit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
